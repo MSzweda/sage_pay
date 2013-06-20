46a94e0abc5e55a4e279ef71199d693ea1301fca
Bart Oleszczyk <bart@primate.co.uk>
2013-06-20 11:36:48 +0100

    three d registration

---
+++ lib/sage_pay/direct/three_d_secure_registration.rb
@@ -0,0 +1,41 @@
+module SagePay
+  module Direct
+    class ThreeDSecureRegistration < SagePay::Utils::Command
+      attr_accessor :pa_res, :md
+
+      validates_presence_of :pa_res, :md
+
+      validates_length_of :pa_res, maximum: 7500
+      validates_length_of :md, maximum: 35
+
+      # TODO: validate format of md and pa_res
+
+      def response_from_response_body(response_body)
+        SagePay::Direct::Response.from_response_body(response_body)
+      end
+
+      def post_params
+        raise ArgumentError, "Invalid transaction registration options (see errors hash for details)" unless self.md.present? && self.pa_res.present?
+        {
+          "MD"        => self.md,
+          "PARes"     => self.pa_res
+        }
+      end
+
+      def url
+        case mode
+        when :direct_simulator
+          "https://test.sagepay.com/Simulator/VSPDirectCallback.asp"
+        when :server_simulator
+          "https://test.sagepay.com/Simulator/VSPDirectCallback.asp"
+        when :test
+          "https://test.sagepay.com/gateway/service/direct3dcallback.vsp"
+        when :live
+          "https://live.sagepay.com/gateway/service/direct3dcallback.vsp"
+        else
+          raise ArgumentError, "Invalid transaction mode"
+        end
+      end
+    end
+  end
+end
